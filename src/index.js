a51b066cd338ff60e8f3b6cd07f0b464b22d225c
Владислав В. Деханов <dekhanov@yamoney.ru>
2020-02-15 12:50:44 +0300

    Немного фиксов

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-import App from './app';
+import {App} from './app';
   
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
