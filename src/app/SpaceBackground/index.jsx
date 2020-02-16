1909cd729c1bee2189c7ebaebc14992b59cb71e2
Владислав В. Деханов <dekhanov@yamoney.ru>
2020-02-16 19:56:07 +0300

    Добавляет кастомное видео на фон

--- src/app/SpaceBackground/index.jsx
+++ src/app/SpaceBackground/index.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import ReactYoutubeBackground from 'react-youtube-background';
 
+import spaceCraftVideo from './assets/moon-astronaut-spacecraft.mov';
 import './styles.css';
 
 export const SpaceBackground = () => {
-    const onReady = ({target: api}) => {
-        api.unloadModule("captions");
-        api.unloadModule("cc");
-    };
     return (
-        <ReactYoutubeBackground 
-            videoId="P5_GlAOCHyE"
-            className="space-background"
-            onReady={onReady}
-            nocookie
-        />
+        <video className="space-background" autoPlay loop>
+            <source src={spaceCraftVideo} type="video/mp4"></source>
+        </video>
     )
 };
