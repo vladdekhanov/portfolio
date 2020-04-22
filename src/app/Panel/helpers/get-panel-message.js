6c94fa66295fa6f3ac7f4ec3dbab68a48a7939a9
Владислав В. Деханов <dekhanov@yamoney.ru>
2020-04-22 11:13:58 +0300

    Add more responsive

--- src/app/Panel/helpers/get-panel-message.js
+++ src/app/Panel/helpers/get-panel-message.js
@@ -1,4 +1,4 @@
 export const getPanelMessage = () =>
     [
-        'Landing on the frontend... ^1000\n`1 November 2013`^1000\n`Vladislav Dekhanov`^1000\n`vladdexx@yandex.ru`',
+        'Landing on the frontend...^1000\n`1 November 2013`^1000\n`Vladislav Dekhanov`^1000\n`vladdexx@yandex.ru`',
     ];
