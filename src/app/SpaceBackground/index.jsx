da1a26f08de310da1ba0a4620be9335c4f3680db
Владислав В. Деханов <dekhanov@yamoney.ru>
2020-04-25 09:25:03 +0300

    mp4 video fallback is added

--- src/app/SpaceBackground/index.jsx
+++ src/app/SpaceBackground/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import video1 from './assets/1.webm';
+import video1mp4 from './assets/1.mp4';
 import video2 from './assets/2.webm';
+import video2mp4 from './assets/2.mp4';
 
 import './styles.css';
 
@@ -11,8 +13,9 @@ import './styles.css';
 export const SpaceBackground = () => {
     return (
         <div className="space-background-container">
-            <video className="space-background" autoPlay loop muted>
+            <video className="space-background" preload="metadata" autoPlay loop muted playsInline>
                 <source src={video1} type="video/webm"></source>
+                <source src={video1mp4} type="video/mp4"></source>
             </video>
         </div>
     )
