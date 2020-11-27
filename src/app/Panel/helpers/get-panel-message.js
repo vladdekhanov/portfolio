4c212bb971fe3c7add1a0ed3e37c523532a6bd14
Владислав В. Деханов <dekhanov@yamoney.ru>
2020-11-27 14:24:11 +0300

    Juggler.dev is added

--- src/app/Panel/helpers/get-panel-message.js
+++ src/app/Panel/helpers/get-panel-message.js
@@ -6,7 +6,7 @@ export const getPanelMessage = () =>
         '`Date: 1 November 2013 (' + format('2013-11-01', 'en_US') + '`)^1000\n' +
         '`Name: Vladislav Dekhanov`^1000\n' +
         '`Contacts: vladdexx@yandex.ru`^1000\n' +
-		'`Current project: [<a target="_blank" href="https://justintime.pw/">JustInTime</a>]`^1000\n' +
+		'`Current project: [<a target="_blank" href="https://juggler.dev/">Juggler.dev</a>]`^1000\n' +
         '`[<a target="_blank" href="https://www.linkedin.com/in/vladislav-dekhanov-87516880/">linkedIn</a>]`' +
         '`[<a target="_blank" href="https://github.com/vladdekhanov">github</a>`]' +
         '`[<a target="_blank" href="https://twitter.com/vladdekhanov_en">twitter</a>]`' +
